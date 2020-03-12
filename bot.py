9ef80490be84d88d87ebeb76976776818d434668
SoraCasus <soracasus@gmail.com>
2020-03-11 19:35:08 -0600

    Added features

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
+import json
 
 from get_memes import get_memes
 from profanity_check import predict, predict_prob
+from random import randrange
 
 bot = discord.Client()
 
@@ -20,9 +22,6 @@ async def on_message(message):
 
     res = predict([text])
 
-    if 1 in res:
-        await message.channel.send('Goodness me, language!')
-
     if 'cuck' in text.lower():
         await message.channel.send('Goodness me, I don\'t know what this... cuck word means but you make it sound like something profane')
 
@@ -31,6 +30,40 @@ async def on_message(message):
 
     if text.startswith('$smite'):
         await message.channel.send('*Smites {}*'.format(text.split()[1]))
+
+    if text.startswith('$oasis_on'):
+        await message.channel.send("Oh, hello there dear")
+        data = {}
+        data['active'] = True
+
+        with open('settings.json', 'w') as outfile:
+            json.dump(data, outfile)
+
+    if text.startswith('$oasis_off'):
+        await message.channel.send("Oh... I'll see myself out then...")
+        data = {}
+        data['active'] = False
+
+        with open('settings.json', 'w') as outfile:
+            json.dump(data, outfile)
+
+    if text.startswith('$meme'):
+        # Scrape meme from reddit
+        get_memes()
+        # Send the meme
+        await message.channel.send("", file=discord.File('meme.png'))
+
+    active = False
+    with open('settings.json') as jsonFile:
+        data = json.load(jsonFile)
+        if bool(data['active']) == True:
+            active = True
+
+    if not active:
+        return
+
+    if 1 in res:
+        await message.channel.send('Goodness me, language!')
 
     if 'bot' in text.lower():
         await message.channel.send('I\'m sorry dear, I believe **you\'re** the "bot" here')
@@ -50,14 +83,12 @@ async def on_message(message):
     if '69420' in text.lower():
         await message.channel.send('The god number!')
 
-    if message.author.name == 'SampleTex':
-        await message.channel.send('Why would you mount a frisbee to your wall')
+    if '666' in text.lower():
+        await message.channel.send("Shhhh, don't say that too loud.... he might hear you!")
 
-    if text.startswith('$meme'):
-        # Scrape meme from reddit
-        get_memes()
-        # Send the meme
-        await message.channel.send("", file=discord.File('meme.png'))
+    if message.author.name == 'SampleTex':
+        if randrange(10) == 1:
+            await message.channel.send('Why would you mount a frisbee to your wall?')
 
     if 'roll' in text.lower():
         await message.channel.send("", file=discord.File('song.mp3'))
